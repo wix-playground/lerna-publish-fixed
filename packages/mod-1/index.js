27fd99fd22aca26a2d234c8020ba637a4bbf3b18
Kobi Nemni <kobin@wix.com>
2019-05-23 16:43:58 +0300

    init

---
+++ packages/mod-1/index.js
@@ -0,0 +1,4 @@
+const mod2 = require("lerna-publish-fixed-mod-2")
+module.exports = {
+    fn2: mod2.fn
+}
---
+++ packages/mod-2/index.js
@@ -0,0 +1,4 @@
+const mod3 = require("lerna-publish-fixed-mod-3")
+module.exports = {
+    fn: () => mod3.fn
+}
